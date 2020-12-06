4ad8b43e5aeecd27cae8b1a2841e808d1f36039a
Harold <harold.goldensky@gmail.com>
2020-12-06 10:37:05 +0800

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+// ==== Menu Toggle ==== //
+function toggleMenu(){
+    const menutoggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('.nav-menu');
+    menutoggle.classList.toggle('active');
+    nav.classList.toggle('active');
+}
+
+
+// ===== Change Color of navitation bar when triggered ==== //
+window.addEventListener('scroll', function(){
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+
+
+// ===== Scroll Reveal Animation ==== //
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+
+});
+
+sr.reveal(`.intro-text, 
+        .intro-img, 
+        .title,
+        .project-card`,{
+    interval: 200
+})
