f11cb9bd5929b54cc5957697f60f64273e9c188b
harika09 <harold.goldensky.com>
2021-05-19 11:10:33 +0800

    added to top btn

--- assets/js/index.js
+++ assets/js/index.js
@@ -43,6 +43,29 @@ for (var i = 0; i < btns.length; i++) { //btn.length getting the range of the li
 }
 
 
+// Scroll To Top
+const  mybutton = document.getElementById('toTopBtn')
+
+window.scroll = function() {scrollfunction()}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function toTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
 // ===== Scroll Reveal Animation ==== //
 
 //Top
